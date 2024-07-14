3d53a963e172f658654b2d8f64452b4e06cd575d
Motassim Khan <motassimkhan4@gmail.com>
2024-07-14 22:06:55 +0530

    DNS

--- DNS.c
+++ DNS.c
@@ -1,26 +1,14 @@
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-    struct hostent *h;
-
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s address\n", argv[0]);
-        exit(1);
+ strcat(fs, x);
+    for (i = 0; i < strlen(a); i++)
+    {
+        t[0] = a[i];
+        t[1] = '\0';
+        if (t[0] == sd)
+        {
+            strcat(fs, s); // Insert ending delimiter before the character
+        }
+        else if(t[0] == ed){
+                strcat(fs,d);
+        }
+        strcat(fs, t); // Insert the character
     }
-
-    if ((h = gethostbyname(argv[1])) == NULL) {
-        perror("gethostbyname");
-        exit(1);
-    }
-
-    printf("Host name : %s\n", h->h_name);
-    printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
-
-    return 0;
-}
